7e119d315ea43af999f8e0b1f676d9cca3ec2823
AlanYHung <AlanYHung@hotmail.com>
2020-11-12 15:47:15 -0800

    Switching Drivers

--- js/catalog.js
+++ js/catalog.js
@@ -4,6 +4,7 @@
 
 // Set up an empty cart for use on this page.
 var cart = new Cart([]);
+var cartCount = 0;
 
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
@@ -25,6 +26,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -34,25 +36,38 @@ function handleSubmit(event) {
 
 }
 
-var testItem;
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
-  var selectedItemEL = document.getElementById('catalog').nodeValue;
-  // var selectedItem = selectedItemEL.value;
-testItem = selectedItemEL;
+  var selectedItem = document.getElementById('items').value;
+  var selectedQuantity = document.getElementById('quantity').value;
+  cart.addItem(selectedItem,selectedQuantity);
 }
 // addSelectedItemToCart();
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  var itemCountCartElement = document.getElementById('itemCount');
+  itemCountCartElement.textContent = cartCount = cart.items.length;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
+  var cartContentsParent = document.getElementById('cartContents');
+  cartContentsParent.innerHTML = '';
+  var ulCartContents = document.createElement('ul');
+  cartContentsParent.appendChild(ulCartContents);
+  var liCartContents;
+
+  for(var i = 0; i < cart.items.length; i++){
+    liCartContents = document.createElement('li');
+    liCartContents.textContent = `${cart.items[i].product} ${cart.items[i].quantity}`
+    ulCartContents.appendChild(liCartContents);
+  }
 }
 
 // Set up the "submit" event listener on the form.
