623796da759f44707801c6f9a0aff02c3236e448
James Gerstenberger <gerstej9@gmail.com>
2020-11-12 16:45:38 -0800

    Working on cart.js

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ Cart.prototype.addItem = function(product, quantity) {
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
   var cartToSave = JSON.stringify(this);
-  localStorage.setItem('Cart', cartToSave);
+  localStorage.setItem('cart', cartToSave);
 };
 
 Cart.prototype.removeItem = function(item) {
--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
 /* global Cart */
 'use strict';
-
+var tBody = document.getElementById('cart-content-table');
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
 var table = document.getElementById('cart');
 table.addEventListener('click', removeItemFromCart);
@@ -19,14 +19,30 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  tBody.innerHTML = '';
+
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
-
   // TODO: Iterate over the items in the cart
+  var tableRowEl;
+  var tableDataEl;
+  var tableDataButton;
+  for( var i = 0; i < cart.items.items.length; i++){
+    tableRowEl = document.createElement('tr');
+    tBody.appendChild(tableRowEl);
+    tableDataEl = document.createElement('td');
+    tableDataButton = document.createElement('button');
+    tableDataButton.textContent = `X ${cart.items.items[i].product} ${cart.items.items[i].quantity}` 
+    tableDataEl.appendChild(tableDataButton);
+    tableDataEl.setAttribute('id',`tde${i}`);
+    tableRowEl.appendChild(tableDataEl);
+  
+  }
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
@@ -34,7 +50,8 @@ function showCart() {
 }
 
 function removeItemFromCart(event) {
-
+  event.preventDefault();
+  console.log(event.target);
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
@@ -43,3 +60,4 @@ function removeItemFromCart(event) {
 
 // This will initialize the page and draw the cart on screen
 renderCart();
+tBody.addEventListener('click', removeItemFromCart);
